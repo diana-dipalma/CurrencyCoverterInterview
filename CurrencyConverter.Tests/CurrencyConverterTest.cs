2fc98570697fa69229f3eea0d5123a95cad72633
Diana DiPalma <diana.dipalma@alaskaair.com>
2022-12-08 19:00:40 -0800

    phuong-tang

--- CurrencyConverter.Tests/CurrencyConverterTest.cs
+++ CurrencyConverter.Tests/CurrencyConverterTest.cs
@@ -1,4 +1,6 @@
-using NUnit.Fåramework;
+using NUnit.Framework;
+using CurrencyConverter;
+using System;
 
 namespace CurrencyConverter.Tests
 {
@@ -9,16 +11,50 @@ namespace CurrencyConverter.Tests
         [SetUp]
         public void Setup()
         {
-            Assert.Fail("NOT IMPLEMENTED");
+            currencyConverter  = new CurrencyConverter();
         }
 
         [Test]
         public void TestConversions()
         {
             Assert.AreEqual(20.56, currencyConverter.GetConvertedAmount("USD", "MXN", 1));
-            Assert.AreEqual(6.36, currencyConverter.GetConvertedAmount("USD", "CAD", 5));
+            Assert.AreEqual(6.35, currencyConverter.GetConvertedAmount("USD", "CAD", 5));
             Assert.AreEqual(0.62, currencyConverter.GetConvertedAmount("MXN", "CAD", 10));
             Assert.AreEqual(0.61, currencyConverter.GetConvertedAmount("DKK", "PLN", 1));
+        }
+
+        [Test]
+        public void TestNotFoundCountry()
+        {
+            Assert.AreEqual(-1, currencyConverter.GetConvertedAmount("POR", "USD", 10));
+            Assert.AreEqual(-1, currencyConverter.GetConvertedAmount("USD", "POR", 10));
+            Assert.AreEqual(-1, currencyConverter.GetConvertedAmount("POR", "POR", 10));
+        }
+
+        [Test]
+        public void TestForNegativeVal()
+        {
+            Assert.AreEqual(-6.35, currencyConverter.GetConvertedAmount("USD", "CAD", -5));
+        }
+
+        [Test]
+        public void TestForSameOrginalCountryAndSameConversionCountry()
+        {
+            Assert.AreEqual(45, currencyConverter.GetConvertedAmount("MXN", "MXN", 45));
+        }
+
+
+        [Test] 
+        public void TestConvertToUSACurrency()
+        {
+            Assert.AreEqual(0.05, currencyConverter.GetConvertedAmount("MXN", "USD", 1));
+        }
+
+
+        [Test]
+        public void TestWithZeroValue()
+        {
+            Assert.AreEqual(0, currencyConverter.GetConvertedAmount("DKK", "PLN", 0));
         }
     }
 }
--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace CurrencyConverter
 {
-    public class Class1
+    public class CurrencyConverter
     {
+        /*
+         * Input the code of orginal country, the code of coverted country and the amount of money
+         * Return the conversion amount between two countries
+         * Return -1 if the country code is not found in the list
+         */
+
+        public Decimal GetConvertedAmount(String OriginalCode, String ConversionCode, Decimal val)
+        {
+            //Get the list of conversion rate 
+            CurrencyConverterRepository res = new CurrencyConverterRepository();
+            IEnumerable<CurrencyConversion> list = res.GetConversions();
+
+            Decimal USDRateForOrg = -1;
+            Decimal USDRateForCon = -1;
+
+            foreach(CurrencyConversion item in list)
+            {
+                //Search in the list to find the currency rate of orignal country vs USA
+                if (item.CountryCode.Equals(OriginalCode))
+                {
+                    USDRateForOrg = item.RateFromUSDToCurrency;
+                }
+
+                //Search in the list to find the currency rate of conversion country vs USA
+                if (item.CountryCode.Equals(ConversionCode))
+                {
+                    USDRateForCon = item.RateFromUSDToCurrency;
+                }
+            }
+
+            //Not found the country in the list
+            if(USDRateForOrg == -1 || USDRateForCon == -1)
+            {
+                return -1;
+            }
+
+            //Total conversion
+            Decimal conversion = (val / USDRateForOrg) * USDRateForCon;
+
+            return Math.Round(conversion,2);
+
+        }
+        
     }
 }
