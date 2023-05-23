5e442f7f73a9b7c89f59a7d403ad150941656c3c
John O'Laughlin <olaughlin@gmail.com>
2023-05-23 08:30:40 -0700

    convert through USD, 2 place rounding. tests pass but no error checking

--- CurrencyConverter.Tests/CurrencyConverterTest.cs
+++ CurrencyConverter.Tests/CurrencyConverterTest.cs
@@ -5,21 +5,21 @@ namespace CurrencyConverter.Tests
 {
     public class CurrencyConverterTest
     {
-        private CurrencyConverter currencyConverter;
+        private CurrencyConverter _currencyConverter;
 
         [SetUp]
         public void Setup()
         {
-            Assert.Fail("NOT IMPLEMENTED");
+            _currencyConverter = new CurrencyConverter();
         }
 
         [Test]
         public void TestConversions()
         {
-            Assert.AreEqual(20.56, currencyConverter.GetConvertedAmount("USD", "MXN", 1));
-            Assert.AreEqual(6.36, currencyConverter.GetConvertedAmount("USD", "CAD", 5));
-            Assert.AreEqual(0.62, currencyConverter.GetConvertedAmount("MXN", "CAD", 10));
-            Assert.AreEqual(0.61, currencyConverter.GetConvertedAmount("DKK", "PLN", 1));
+            Assert.AreEqual(20.56, _currencyConverter.GetConvertedAmount("USD", "MXN", 1));
+            Assert.AreEqual(6.35, _currencyConverter.GetConvertedAmount("USD", "CAD", 5));
+            Assert.AreEqual(0.62, _currencyConverter.GetConvertedAmount("MXN", "CAD", 10));
+            Assert.AreEqual(0.61, _currencyConverter.GetConvertedAmount("DKK", "PLN", 1));
         }
     }
 }
--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CurrencyConverter
 {
     public class CurrencyConverter
     {
+        private readonly Dictionary<(string, string), Decimal> _conversions;
+
         public CurrencyConverter()
         {
-            throw new NotImplementedException();
+            _conversions = new Dictionary<(string, string), Decimal>();
+            var usd_conversions = new CurrencyConverterRepository().GetConversions();
+            foreach (var conv1 in usd_conversions)
+            {
+                foreach (var conv2 in usd_conversions)
+                {
+                    Decimal rate = conv2.RateFromUSDToCurrency / conv1.RateFromUSDToCurrency;
+                    _conversions[(conv1.CountryCode, conv2.CountryCode)] = rate;
+                }
+            }
         }
-        
-        public double GetConvertedAmount(string fromCurrency, string toCurrency, double amount)
+
+        public Decimal GetConvertedAmount(string fromCurrency, string toCurrency, Decimal amount)
         {
-            return 0.0;
+            var rate = GetConversionRate(fromCurrency, toCurrency);
+            return Math.Round(amount * rate, 2);
+        }
+
+        private Decimal GetConversionRate(string fromCurrency, string toCurrency)
+        {
+            return _conversions[(fromCurrency, toCurrency)];
         }
     }
 }
