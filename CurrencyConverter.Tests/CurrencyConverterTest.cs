00c4d4355e989621382662d1a3c807404e075582
John O'Laughlin <olaughlin@gmail.com>
2023-05-22 23:14:54 -0700

    Test builds and runs (but fails)

--- CurrencyConverter.Tests/CurrencyConverterTest.cs
+++ CurrencyConverter.Tests/CurrencyConverterTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using CurrencyConverter;
 
 namespace CurrencyConverter.Tests
 {
--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -2,7 +2,16 @@
 
 namespace CurrencyConverter
 {
-    public class Class1
+    public class CurrencyConverter
     {
+        public CurrencyConverter()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public double GetConvertedAmount(string fromCurrency, string toCurrency, double amount)
+        {
+            return 0.0;
+        }
     }
 }
